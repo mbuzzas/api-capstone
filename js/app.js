f736e933ca3f3174779c7a8c22d63563ef61bb67
mbuzzas <mbuzzas3@gmail.com>
2016-11-12 08:42:18 -0800

    adds recipe search

--- js/app.js
+++ js/app.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
 	
-	var userSearch = '';
+	$('.ing-specify form').submit(function(event){
+		event.preventDefault();
+		var add = $(this).find('input[type="text"]').val();
+		var html = '<li class="i-list">' + 
+					'<span class="i-text">'+add+'</span>' +
+					'<span class="right-check">&#10005;</span>' +
+				'</li>';
+		$(this).siblings('ul').append(html);
+		$(this).find('input[type="text"]').val('');
+	});
 
-	var getRecipe = function(keyword,additions,subtractions) {
+	$('.recipe-search').submit(function(event){
+		event.preventDefault();
+		var keyword = $('.recipe-search_input').val();
+		var additions = $('.ingredient-addition').val();
+		var subtractions = $('.ingredient-subtraction').val();
+		getRecipe();
+		$('.recipe-search_input').val('');
+	})
+
+	var getRecipe = function(keyword, additions, subtractions) {
 
 		var baseURL = 'https://api.yummly.com/v1/api/recipes';
 		var query = {
-			q: keyword,
+			q: keyword + '&allowedIngredient[]=' + additions + '&excludedIngredient[]=' + subtractions,
 			_app_id: 'ecc8e08e',
 			_app_key: '1f1523599d47bfaebe84f01732917a96',
 		};
@@ -16,8 +34,16 @@ $(document).ready(function(){
 			dataType: 'jsonp',
 			type: 'GET'
 		})
-	}
+		.done(function(result){
+			$('.recipe-list').html('');
+			$.each(result.matches, function(i, match) {
+				console.log(match)
+			});
+		})
+		.fail(function(jqXHR, textStatus, errorThrown){
+		});
+	};
 
 
-	$('')
-})
+
+});
